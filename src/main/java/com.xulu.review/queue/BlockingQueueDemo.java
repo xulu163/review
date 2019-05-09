40c81648c6a30035fda37e383aed3b2c2c9576df
xulu572267298 <572267298@qq.com>
2019-05-09 14:51:15 +0800

    阻塞队列

---
+++ src/main/java/com.xulu.review/queue/BlockingQueueDemo.java
@@ -0,0 +1,22 @@
+package com.xulu.review.queue;
+
+/**
+ * 阻塞队列
+ *
+ * @author xulu  E-mail:java_xul@163.com
+ * @version 1.0
+ * @create 2019-05-09 12:26
+ **/
+public class BlockingQueueDemo {
+
+    /**
+     * 1 队列
+     * 2 阻塞队列
+     *  2.1阻塞队列有没有好的一面
+     *  2.2不得不阻塞，你如何管理
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
